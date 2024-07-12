48ddd185b3b5175d02448a6e634a393795a328c0
TranAnhVu0411 <vuta183674@gmail.com>
2024-07-12 14:35:22 +0700

    Add DB Connection

--- fullstack_dotnet_web_development/api/Program.cs
+++ fullstack_dotnet_web_development/api/Program.cs
@@ -1,9 +1,17 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("AppDbConnectionString");
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 var app = builder.Build();
 
